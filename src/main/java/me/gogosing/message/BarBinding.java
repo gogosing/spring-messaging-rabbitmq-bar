e46e56d398c8ec4522aa988e7d80bc98c93ec4b1
JinBum Jeong <gogosing@naver.com>
2020-03-02 15:22:44 +0900

    eureka 의존성 제거

--- src/main/java/me/gogosing/message/BarBinding.java
+++ src/main/java/me/gogosing/message/BarBinding.java
@@ -10,18 +10,18 @@ import org.springframework.messaging.SubscribableChannel;
  */
 public interface BarBinding {
 
-    String Publish = "barOutput";
-
     String Subscribe = "barInput";
 
+    String Publish = "barOutput";
+
     /**
-     * bar service 의 output 메시지를 listen.
+     * 다른 서비스의 output 메시지를 listen.
      */
     @Input(Subscribe)
     SubscribableChannel subscribe();
 
     /**
-     * bar service 에 메시지 발신.
+     * 다른 서비스에 메시지 발신.
      */
     @Output(Publish)
     MessageChannel publish();
